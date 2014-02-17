85e0474acb97be4c7cf7510679ac08600fda237f
BrainCrumbz <admin@braincrumbz.com>
2014-02-17 02:09:45 +0100

    Reverted URLs to jarred.github.com

--- js/app/bookmarklet.js
+++ js/app/bookmarklet.js
@@ -1,7 +1,7 @@
 (function() {
   var analyticsID, checkForRequire, libs, loadLibs, server, sourceImage;
 
-  server = '//raw.github.com/BrainCrumbz/src-img/master';
+  server = 'http://jarred.github.com/src-img/';
 
   analyticsID = 'UA-4516491-29';
 
